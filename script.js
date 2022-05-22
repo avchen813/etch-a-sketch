2120575101787334ef993320f26b3070d10080aa
avchen <anthony.chen813@gmail.com>
2022-05-21 23:04:56 -0400

    Modified JS to set background of each cell individually. Removed CSS variables and background color class that was previously used to dictate the background color of all cells.

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-const mainContainer = document.querySelector('.main-container');
+const mainContainer = document.querySelector(".main-container");
 const root = document.documentElement;
 
-
-let gridNumberOfColumns = prompt('How many columns would you like your grid to have?');
+let gridNumberOfColumns = prompt(
+  "How many columns would you like your grid to have?"
+);
 appendCells();
-// Set number of columns based on gridNumberOfColumns value
-root.style.setProperty('--number-of-columns', gridNumberOfColumns);
-
-
-
-function changeColor(cell) {
-    const cells = document.querySelectorAll('.grid-cell');
-    cells.forEach(cell => {
-        cell.addEventListener('mouseenter', event => {
-            root.style.setProperty('--r-color-value', Math.floor(Math.random()*256));
-            root.style.setProperty('--g-color-value', Math.floor(Math.random()*256));
-            root.style.setProperty('--b-color-value', Math.floor(Math.random()*256));
-            cell.classList.add('cell-color');
-        })
-    })
-}
-
 
 // Create and append cells to container based on gridNumberOfColumns value
-function appendCells () {
-    for (i = 0; i < Math.pow(gridNumberOfColumns, 2); i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('grid-cell');
-        mainContainer.appendChild(cell);    
-    }
+function appendCells() {
+  // Set number of columns based on gridNumberOfColumns value for CSS grid
+  root.style.setProperty("--number-of-columns", gridNumberOfColumns);
 
-    changeColor();
+  for (i = 0; i < Math.pow(gridNumberOfColumns, 2); i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("grid-cell");
+
+    mainContainer.appendChild(cell);
+  }
+  changeColor();
+}
+
+function changeColor() {
+  let cells = document.querySelectorAll(".grid-cell");
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", (event) => {
+      let redValue = Math.floor(Math.random() * 256);
+      let greenValue = Math.floor(Math.random() * 256);
+      let blueValue = Math.floor(Math.random() * 256);
+      cell.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+    });
+  });
 }
