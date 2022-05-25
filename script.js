57aa59ec8f22448e7ada8ef128c54e1cc1746506
avchen <anthony.chen813@gmail.com>
2022-05-25 00:52:38 -0400

    Added validation logic to ensure that the prompt only allows for integers between 1 and 64. Added a button to reset the grid and prompt the player again.

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 const mainContainer = document.querySelector(".main-container");
 const root = document.documentElement;
 
-let gridNumberOfColumns = prompt(
-  "How many columns would you like your grid to have?"
-);
-appendCells();
+let gridSquaresOnEachSide;
+createGrid();
 
-// Create and append cells to container based on gridNumberOfColumns value
+function createGrid() {
+  gridSquaresOnEachSide = parseInt(prompt(
+    "How many squares would you like your grid to have on each side (max. 64)?"
+  ));
+  
+  if (gridSquaresOnEachSide === null) {
+    alert("Canceled");
+  } else if (gridSquaresOnEachSide > 0 && gridSquaresOnEachSide <= 64 && gridSquaresOnEachSide % 1 === 0) {
+    resetGrid();
+    appendCells();
+  } else {
+    alert("Invalid entry. Please enter a whole number from 1 to 64.");
+  }
+}
+
+function resetGrid() {
+  mainContainer.innerHTML = '';
+}
+
+
+
+// Create and append cells to container based on gridSquaresOnEachSide value
 function appendCells() {
-  // Set number of columns based on gridNumberOfColumns value for CSS grid
-  root.style.setProperty("--number-of-columns", gridNumberOfColumns);
+  // Set number of squares on each side based on gridSquaresOnEachSide value for CSS grid
+  root.style.setProperty("--squares-on-each-side", gridSquaresOnEachSide);
 
-  for (i = 0; i < Math.pow(gridNumberOfColumns, 2); i++) {
+  for (i = 0; i < Math.pow(gridSquaresOnEachSide, 2); i++) {
     const cell = document.createElement("div");
     cell.classList.add("grid-cell");
 
